e5d37c5e794ac53e9a2386ca9bfd793a2e03a1d0
maveys <bui.chrisk@gmail.com>
2017-01-16 02:21:29 -0600

    update ajax call

--- loadimage.js
+++ loadimage.js
@@ -48,7 +48,7 @@ $(document).ready(function() {
 		var file_ext = {};
         file_ext[0]=".png";
 		$.ajax({
-			url: 'https://maveys.github.io/assets/girls/'+get_idol_group()+'/'+$("#idol-list").val(),
+			url: 'https://maveys.github.io/assets/girls/'+get_idol_group()+'/'+$("#idol-list").val()+'/',
 			success: function(data) {
 				//$("#image-list").append('<ul>');
 				//$("#bg-list").hide();
