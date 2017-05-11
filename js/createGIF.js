3ea8335b48bd3420b069a300bdcbf92aedf8ce0d
maveys <bui.chrisk@gmail.com>
2017-05-10 21:58:08 -0500

    fixed no text background

--- js/createGIF.js
+++ js/createGIF.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 
     var encoder = new GIFEncoder();
     encoder.setRepeat(1); // repeat once
-    encoder.setDelay(100); // every 1500 milliseconds new frame
+    encoder.setDelay(50); // every 1500 milliseconds new frame
     encoder.start();
 
     $("#addFrame").click(function() {
@@ -33,23 +33,39 @@ $(document).ready(function() {
         context.shadowOffsetX = 1;
         context.shadowOffsetY = 2;
         context.scale(1,1);
+
+        if($("#speaker").val() === "-1") {
+            $("#error").html("*Please select a speaker.");
+            return;
+        }
         add_idol_name();
-        for (var i = 0; i < text_1.length; i++) {
-            context.fillText(text_1[i], 125 + (i * 15)  , 510);
-            encoder.addFrame(context);
-        }
-        for (var i = 0; i < text_2.length; i++) {
-            context.fillText(text_2[i], 125 + (i * 15), 545);
-            encoder.addFrame(context);
-        }
-        for (var i = 0; i < text_3.length; i++) {
-            context.fillText(text_3[i], 125 + (i * 15), 580)
-            encoder.addFrame(context);
-        }
-        for (var i = 0; i < 10; i++) {
-            encoder.addFrame(context);
-        }
-        context.restore();
+        var image = new Image();
+        var canvas = document.getElementById('scene');
+        var context = canvas.getContext('2d');
+        render_canvas();
+        image.src = "https://maveys.github.io/assets/sprites/text.png";
+
+        image.onload = function() {
+            context.drawImage(image, 50,640 - 175, image.width - 100, image.height);
+            for (var i = 0; i < text_1.length; i++) {
+                context.fillText(text_1[i], 125 + (i * 15)  , 510);
+                encoder.addFrame(context);
+            }
+            for (var i = 0; i < text_2.length; i++) {
+                context.fillText(text_2[i], 125 + (i * 15), 545);
+                encoder.addFrame(context);
+            }
+            for (var i = 0; i < text_3.length; i++) {
+                context.fillText(text_3[i], 125 + (i * 15), 580)
+                encoder.addFrame(context);
+            }
+            for (var i = 0; i < 10; i++) {
+                encoder.addFrame(context);
+            }
+            context.restore();
+        };
+        $("#error").html("");
+        alert("frame added");
     });
 
     $("#gif").click(function() {
